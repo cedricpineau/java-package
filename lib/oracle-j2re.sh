a2dceeab3f4951ab7fe39ecb8cafda440af4065f
Cédric Pineau <cedric.pineau@gmail.com>
2011-12-23 15:04:43 +0100

    support for 32 bits java oracle 7

--- lib/oracle-j2re.sh
+++ lib/oracle-j2re.sh
@@ -5,6 +5,11 @@ function oracle_j2re_detect() {
   case "${DEB_BUILD_ARCH:-$DEB_BUILD_GNU_TYPE}" in
     i386|i486-linux-gnu)
       case "$archive_name" in
+	"jre-7u"[0-9]"-linux-i586.tar.gz") # SUPPORTED
+	    j2se_version=1.7.0+update${archive_name:6:1}${revision}
+	    j2se_expected_min_size=94 #Mb
+	    found=true
+	    ;;
       esac
       ;;
     amd64|x86_64-linux-gnu)
